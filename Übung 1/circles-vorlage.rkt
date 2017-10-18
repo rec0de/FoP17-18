4d98f94fd1071fee24f0c30feb81c5e176f768c3
Kumiho <kumiho@rec0de.net>
2017-10-18 21:10:12 +0200

    Add U1 skeleton

---
+++ Übung 1/circles-vorlage.rkt
@@ -0,0 +1,34 @@
+;; Exercise 7.1
+;; distance-centers:
+;; Explanation: 
+;; Example: 
+
+(define (distance-centers x1 y1 x2 y2)
+  
+)
+
+;; Tests
+
+
+;; Exercise 7.2
+;; circles-position: 
+;; Explanation: 
+;; Example: 
+
+(define (circles-position x1 y1 r1 x2 y2 r2)
+  
+)
+
+;; Tests
+
+
+;; Exercise 7.3
+;; calculate-properties: 
+;; Explanation: 
+;; Example: 
+
+(define (calculate-properties x y r s)
+
+)
+
+;; Tests
