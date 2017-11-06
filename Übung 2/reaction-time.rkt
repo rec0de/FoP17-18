3d6c977376343b991bbe409fc2401fc37a6c7bc3
Kumiho <kumiho@rec0de.net>
2017-11-06 19:37:47 +0100

    Initial solution for 6.1 to 6.3

---
+++ Übung 2/reaction-time.rkt
@@ -0,0 +1,162 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname reaction-time) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct person (age sex code))
+
+(define-struct subject (person times))
+
+(define VP01 (make-subject (make-person 22 'm 'MW17K) (list 220 301 189 272 311)))
+(define VP02 (make-subject (make-person 25 'f 'MP25G) (list 234 197 253 257 206)))
+(define VP03 (make-subject (make-person 23 'f 'CT03R) (list 197 202 214 222 233)))
+(define VP04 (make-subject (make-person 20 'm 'MM09R) (list 273 314 257 264 217)))
+(define VP05 (make-subject (make-person 19 'm 'KR22I) (list 198 197 228 253 199)))
+(define VP06 (make-subject (make-person 26 'm 'FR01B) (list 212 204 289 294 223)))
+(define VP07 (make-subject (make-person 28 'f 'RA15R) (list 258 323 189 247 303)))
+(define VP08 (make-subject (make-person 22 'm 'RP18R) (list 221 307 182 271 316)))
+(define VP09 (make-subject (make-person 24 'f 'GH31W) (list 230 295 304 264 237)))
+(define VP10 (make-subject (make-person 19 'f 'OM29Q) (list 299 194 242 303 243)))
+
+(define subjects (list VP01 VP02 VP03 VP04 VP05 VP06 VP07 VP08 VP09 VP10))
+
+;; Helper functions
+
+;; Re-implementing max and min to avoid forced conversion to float when comparing with +inf
+
+;; math-min: number number -> number
+;; Explanation: Returns the smaller of two given numbers
+;; Example: (math-min 8 4) -> 4
+
+(define (math-min a b)
+  (if (< a b) a b)
+)
+
+;; Tests
+(check-expect (math-min 500 499) 499)
+(check-expect (math-min -5 1) -5)
+(check-expect (math-min -3.14 -4.2) -4.2)
+
+;; math-max: number number -> number
+;; Explanation: Returns the larger of two given numbers
+;; Example: (math-max 8 4) -> 8
+
+(define (math-max a b)
+  (if (< a b) b a)
+)
+
+;; Tests
+(check-expect (math-max 500 499) 500)
+(check-expect (math-max -5 1) 1)
+(check-expect (math-max -3.14 -4.2) -3.14)
+
+;; subject-age: subject -> number
+;; Explanation: Returns age of a given subject
+;; Example: (subject-age (make-subject (make-person 19 'f 'OM29Q)) -> 19
+(define (subject-age input-subject)
+  (person-age (subject-person input-subject))
+)
+
+;; Tests
+(check-expect (subject-age VP01) 22)
+(check-expect (subject-age VP09) 24)
+
+;; gender-to-number: subject -> number
+;; Explanation: Returns -1 if given subject is male (or unrecognized), 1 if subject is female
+;; Example: (gender-to-number VP02) -> 1
+(define (gender-to-number input-subject)
+  (if (symbol=? (person-sex (subject-person input-subject)) 'f) 1 -1)
+)
+
+;; Tests
+(check-expect (gender-to-number VP01) -1)
+(check-expect (gender-to-number VP02) 1)
+
+;; list-sum: (listof number) -> number
+;; Explanation: Calculates sum of given list of numbers
+;; Example: (list-sum (list 10 1 1 4)) -> 16
+(define (list-sum input)
+  (if (empty? input) 0 (+ (first input) (list-sum (rest input))))
+)
+
+;; Tests
+(check-expect (list-sum (list 1 2 3)) 6)
+(check-expect (list-sum (list 100, 20, 500, 4)) 624)
+
+;; gender-overhang: (listof subject) -> number
+;; Explanation: Returns difference in number of female to male subjects - positive n means n more female subjects, negative n means n more male subjects. 0 if male and female are balanced
+;; Example: (gender-overhang (list VP01 VP02 VP03)) -> 1
+(define (gender-overhang subjects)
+  (cond
+    [(empty? subjects) 0]
+    [else (+ (gender-overhang (rest subjects)) (gender-to-number (first subjects)))]
+  )
+)
+
+;; Tests
+(check-expect (gender-overhang (list VP01 VP02)) 0)
+(check-expect (gender-overhang (list VP02 VP03)) 2)
+(check-expect (gender-overhang (list VP01 VP04 VP05 VP02)) -2)
+(check-expect (gender-overhang empty) 0)
+
+;; Exercise 6.1
+
+;; youngest-oldest-subject: (listof subject) symbol -> number
+;; Explanation: Returns age of oldest / youngest subject in list according to symbol 'youngest or 'oldest
+;;              Returns oldest age 0 and youngest age +infinity for empty lists
+;; Example: (youngest-oldest-subject subjects 'oldest) -> 28
+(define (youngest-oldest-subject people comparator)
+  (cond
+    [(empty? people) (if (symbol=? comparator 'oldest) 0 +inf.0)]
+    [else (if (symbol=? comparator 'oldest) (math-max (subject-age (first people)) (youngest-oldest-subject(rest people) 'oldest)) (math-min (subject-age (first people)) (youngest-oldest-subject(rest people) 'youngest)))]
+  )
+)
+
+;; Tests
+(check-expect (youngest-oldest-subject subjects 'oldest) 28)
+(check-expect (youngest-oldest-subject subjects 'youngest) 19)
+(check-expect (youngest-oldest-subject empty 'oldest) 0)
+
+;; Exercise 6.2
+
+;; balanced-sex: (listof subject) -> boolean
+;; Explanation: Checks if gender distribution of given subject list is balaced. Returns true if number of male and female subjects is equal, false otherwise
+;; Example: (balanced-sex (list VP01 VP02 VP03)) -> false
+(define (balanced-sex people)
+  (zero? (gender-overhang people))
+)
+
+;; Tests
+(check-expect (balanced-sex (list VP01 VP02)) true)
+(check-expect (balanced-sex (list VP02 VP03)) false)
+(check-expect (balanced-sex (list VP01 VP04 VP05 VP02)) false)
+(check-expect (balanced-sex empty) true)
+
+;; Exercise 6.3
+
+;; mean-of-subject: (listof subject) -> number
+;; Explanation: Scans list of subjects for subject with matching ID code and calculates, if found, the average response time for that subject
+;; Example: (mean-of-subject subjects 'MW17K) -> 258.6
+(define (mean-of-subject people code)
+  (cond
+    [(empty? people) false] ;; Subject code not found in list
+    ;; If subject code is found, sum up list of times and divide by list length
+    [(symbol=? (person-code (subject-person (first people))) code) (/ (list-sum (subject-times (first people))) (length (subject-times (first people))))]
+    ;; Otherwise, check rest of list
+    [else (mean-of-subject (rest people) code)]
+  )
+)
+
+;; Tests
+(check-within (mean-of-subject subjects 'MW17K) 258.6 0.0001)
+(check-expect (mean-of-subject subjects 'MP25G) 229.4 0.0001)
+
+;; Exercise 6.4
+
+;; std-of-subject:
+;; Explanation:
+;; Example:
+(define (std-of-subject people code)
+  true
+)
+
+;; Tests
+
