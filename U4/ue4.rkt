450b83b34963612ff1bacecea144191334a640be
rec0de <mail@rec0de.net>
2017-11-15 14:58:50 +0100

    Added base conversion and string to encodeable functions

---
+++ U4/ue4.rkt
@@ -0,0 +1,73 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ue4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp")) #f)))
+;; The alphabet we use is encoded in eight bits and the numbers correspond to ASCII symbols.
+;; ASCII number 27 "\e" is used as termination symbol.
+
+;; convert-to-base-four: number -> (listof number)
+;; Explanation: Converts positive (strictly > 0) base10 integer numbers to their base4 representation, returned as a list of numbers representing single digits
+;; Example: (convert-to-base-four 4) -> (list 1 0)
+(define (convert-to-base-four num)
+  (cond
+    [(zero? num) empty]
+    [else (append (convert-to-base-four (quotient num 4)) (list (modulo num 4)))]
+  )
+)
+
+;; Tests
+(check-expect (convert-to-base-four 10) (list 2 2)) ;; pre-defined
+(check-expect (convert-to-base-four 100) (list 1 2 1 0)) ;; pre-defined
+(check-expect (convert-to-base-four 812) (list 3 0 2 3 0))
+;;(check-expect (convert-to-base-four 0) (list 0))
+
+;; string->encodeable: string -> (listof (listof number))
+;; Explanation:
+;; Example: (string->encodeable "ABC") -> (list (list 1 0 0 1) (list 1 0 0 2) (list 1 0 0 3))
+(define (string->encodeable s)
+  (map (lambda (s) (convert-to-base-four (string->int s))) (explode s))
+)
+
+;; Tests
+(check-expect (string->encodeable "fop") (list (list 1 2 1 2) (list 1 2 3 3) (list 1 3 0 0))) ;; pre-defined
+(check-expect (string->encodeable "FOP") (list (list 1 0 1 2) (list 1 0 3 3) (list 1 1 0 0))) ;; pre-defined
+(check-expect (string->encodeable "F4ncY S#1t") (list (list 1 0 1 2) (list 3 1 0) (list 1 2 3 2) (list 1 2 0 3) (list 1 1 2 1) (list 2 0 0) (list 1 1 0 3) (list 2 0 3) (list 3 0 1) (list 1 3 1 0)))
+(check-expect (string->encodeable "") empty)
+
+;; encodable->string: (listof (listof number)) -> string
+;; Explanation:
+;; Example: 
+(define (encodeable->string lon)
+  true
+)
+
+;;(check-expect (encodeable->string (list (list 1 2 1 2) (list 1 2 3 3) (list 1 3 0 0))) "fop")
+;;(check-expect (encodeable->string (list (list 1 0 1 2) (list 1 0 3 3) (list 1 1 0 0))) "FOP")
+
+;; load-image: string -> (list of color)
+;; Loads the image given by path-to-image and converts it to a list of color.
+;; Example: (load-image "example.png") -> (list (color 128 255 32 255) ...)
+(define (load-image path-to-image)
+  true
+;;  (image->color-list (bitmap/file path-to-image))
+  )
+
+;; store-image: (list of color) string number number -> boolean
+;; Saves the image in PNG format at path-to-image.
+;; loc: List of color which is supposed be stored in PNG format.
+;; path-to-image: Relative path to the location where the image should be saved.
+;; width: The width of the image.
+;; heigth: The height of the image. 
+(define (store-image loc path-to-image width height)
+  true
+;;  (save-image (color-list->bitmap loc width height) path-to-image)
+  )
+
+
+(define (steganographie-enc loc m k)
+  true
+)
+
+
+(define (steganographie-dec loc k)
+  true
+)
