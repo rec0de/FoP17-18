2507a746a061e40e1a6445c8090c742e622c571b
rec0de <mail@rec0de.net>
2017-12-01 21:40:48 +0100

    Added performance optimized code version, Expanded documentation, Fixed gradient generation not working with some image sizes

--- U5/ue5.rkt
+++ U5/ue5.rkt
@@ -8,9 +8,9 @@
 ;; Number of grid cells
 (define SCALE 64)
 ;; The width of the grid
-(define GWIDTH (/ WIDTH SCALE))
+(define GWIDTH (ceiling (/ WIDTH SCALE))) ;; Modified to allow image sizes where height / width aren't multiples of scale
 ;; The height of the grid
-(define GHEIGHT (/ HEIGHT SCALE))
+(define GHEIGHT (ceiling (/ HEIGHT SCALE))) ;; Modified to allow image sizes where height / width aren't multiples of scale
 
 ;; Defines a 2D Vector
 ;; x - The x-value of the vector
@@ -54,7 +54,7 @@
 (check-within (fade 0.8) 0.94208 0.001)
 
 ;; Globally store the list of gradients
-(define gradients (create-gradients (* (+ 1 GWIDTH) (+ 1 GHEIGHT))))
+(define gradients (create-gradients (ceiling(* (+ 1 GWIDTH) (+ 1 GHEIGHT))))) ;; Modified to allow image sizes where height / width aren't multiples of scale
 
 
 ;; linear-interpolation: number number number -> number
@@ -69,7 +69,7 @@
 (check-expect (linear-interpolation 5 10 0.1) 5.5)
 
 ;; nth-element: (listof X) number -> X
-;; Explanation: Returns the n-th element of given list, starting at zero. Error on out-of-bounds index
+;; Explanation: Returns the n-th element of given list, starting at zero. Error on out-of-bounds index. Slooooooooow.
 ;; Example: (nth-element (list 1 2 3) 0) -> 1
 (define (nth-element lst index)
   (cond
@@ -95,6 +95,7 @@
 
 ;; dot-grid-gradient: number number number number -> number
 ;; Explanation: Calculates scalar product of a gradient specified by coordinates and the vector between that gradient and the specified pixel
+;;              Parameter order is pixel_x pixel_y gradient_x gradient_y
 ;; Example: (dot-grid-gradient 30 30 0 0) -> -23.96...
 (define (dot-grid-gradient ix iy x y)
   (local
@@ -111,13 +112,14 @@
 ;; (not specified)
 
 ;; perlin-noise: number number -> number
-;; Explanation: Calculates the perlin-noise value for a given pixel by looking up all corner gradients and interpolating values
+;; Explanation: Calculates the perlin-noise value for a given pixel by looking up all corner gradients and interpolating values according to given formula
 ;;              Output is scaled to range 0-1
 ;; Example: (not specified)
 (define (perlin-noise x y)
   (local
     ((define rx (/ x SCALE))
      (define ry (/ y SCALE))
+     ;; Calculate corner values
      (define top-left (dot-grid-gradient rx ry (floor rx) (floor ry)))
      (define top-right (dot-grid-gradient rx ry (ceiling rx) (floor ry)))
      (define bot-left (dot-grid-gradient rx ry (floor rx) (ceiling ry)))
@@ -173,3 +175,143 @@
 ;; (not specified)
 
 ;;(save-image (color-list->bitmap (create-land WIDTH HEIGHT) WIDTH HEIGHT) "land.png")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance optimized functions below    ;;
+;; Not really relevant, but cool either way ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; quick-dot-grid-gradient: number number number number vector2d -> number
+;; Explanation: Calculates scalar product of a gradient given as a vector and the vector between that gradient and the specified pixel (saves time by not looking up gradient by coords)
+;; Example: (quick-dot-grid-gradient 30 30 0 0 (make-vector2d #i0.5107958369498081 #i0.8597020489418092)) -> 41.11...
+(define (quick-dot-grid-gradient ix iy x y gradient)
+  (scalar-mult gradient (make-vector2d (- ix x) (- iy y)))
+)
+;; Tests
+;; (not specified)
+
+;; perlin-noise-on-steroids: number number vector2d vector2d vector2d vector2d -> number
+;; Explanation: Calculates the perlin-noise value for a given pixel using given corner gradients to reduce runtime
+;;              Output is scaled to range 0-1
+;; Example: (not specified)
+(define (perlin-noise-on-steroids rx ry corner-top-left corner-top-right corner-bot-left corner-bot-right)
+  (local
+     ((define frx (floor rx))
+     (define fry (floor ry))
+     (define crx (ceiling rx))
+     (define cry (ceiling ry))
+     (define top-left (quick-dot-grid-gradient rx ry frx fry corner-top-left))
+     (define top-right (quick-dot-grid-gradient rx ry crx fry corner-top-right))
+     (define bot-left (quick-dot-grid-gradient rx ry frx cry corner-bot-left))
+     (define bot-right (quick-dot-grid-gradient rx ry crx cry corner-bot-right))
+     (define top (linear-interpolation top-left top-right (fade (- rx frx))))
+     (define bot (linear-interpolation bot-left bot-right (fade (- rx frx))))
+     (define final (linear-interpolation top bot (fade (- ry fry))))
+    )
+    (+ (/ final 2) 0.5) ;; Scale final value from -1..1 to 0..1
+  ) 
+)
+;; Tests
+;; (not specified)
+
+;; create-land-and-hurry: number number -> (listof color)
+;; Explanation: Creates a list of colors representing a width*height image colored according to the perlin-noise value of each pixel.
+;;              Makes heavy use of caching to cut down runtime using perlin-noise-on-steroids. Code is barely readable, but works.
+;;              Performs about 4.5x better than regular perlin noise for 512x512 images. Output should be identical to regular create-land
+;; Example: (not specified)
+(define (create-land-and-hurry width height)
+  (local
+    (
+     ;; Color definitions
+     (define deepsea (make-color 78 255 239));24 120 177))
+     (define sea (make-color 78 255 239));32 164 243))
+     (define beach (make-color  120 227 253));244 208 111))
+     (define grasslands (make-color 169 237 253));122 229 130))
+     (define grasslands2 (make-color 255 238 238));100 188 107))
+     (define grasslands3 (make-color 255 187 221));78 146 83))
+     (define mountains (make-color 255 119 153));52 052 52))
+     (define snow (make-color 255 143 171));200 200 200))
+     ;; number2color: number -> color
+     ;; Explanation: Converts a number between 0 and 1 to a color by interpreting the number as terrain height
+     ;; Example: (number2color 0.4) -> beach -> (make-color 244 208 111)
+     (define (number2color num)
+       (cond
+         [(< num 0.32) deepsea] ;; Deep Sea
+         [(< num 0.38) sea] ;; Sea
+         [(< num 0.45) beach] ;; Beach
+         [(< num 0.52) grasslands] ;; Grasslands
+         [(< num 0.59) grasslands2] 
+         [(< num 0.65) grasslands3]
+         [(< num 0.74) mountains] ;; Mountains
+         [else snow]
+       )
+     )
+     ;; coords2gradient: number number -> vector2d
+     ;; Explanation: Returns gradient of a given grid intersection by looking up in gradients list
+     ;; Example: (coords2gradient 2 1) -> [random gradient vector at list position 17]
+     (define (coords2gradient x y)
+       (nth-element gradients (+ (* y GWIDTH) x)) ;; index of gradient is x + y*gradients_per_row
+     )
+     ;; Cached corner gradients initial definition
+     (define cur-top-left  (coords2gradient 0 0))
+     (define cur-top-right (coords2gradient 1 0))
+     (define cur-bot-left  (coords2gradient 0 1))
+     (define cur-bot-right (coords2gradient 1 1))
+     (define cur-x 0.5)
+     (define cur-y 0.5)
+     ;; recalc: -> undefined
+     ;; Explanation: Recalculates all cached corner gradients according to cur-x and cur-y
+     ;; Example: (recalc!) -> undefined
+     (define (recalc!)
+       (begin
+         (set! cur-top-left  (coords2gradient (floor cur-x) (floor cur-y)))
+         (set! cur-top-right (coords2gradient (ceiling cur-x) (floor cur-y)))
+         (set! cur-bot-left  (coords2gradient (floor cur-x) (ceiling cur-y)))
+         (set! cur-bot-right  (coords2gradient (ceiling cur-x) (ceiling cur-y)))
+       )
+     )
+     ;; inc-x: number -> undefined
+     ;; Explanation: Increments x value used for calculation of cached gradients or resets to zero if the x value of the current pixel wrapped around to zero
+     ;;              newx is x value of currently processed pixel divided by SCALE
+     ;; Example: (inc-y) -> undefined
+     (define (inc-x! newx)
+       (begin
+         (set! cur-x (+ (floor newx) 0.5))
+         (recalc!)
+       )
+     )
+     ;; inc-y: -> undefined
+     ;; Explanation: Increments y value used for calculation of cached gradients by one, resets x value and recalculates cached gradients
+     ;; Example: (inc-y) -> undefined
+     (define (inc-y!)
+       (begin
+         (set! cur-y (+ cur-y 1))
+         (set! cur-x 0.5)
+         (recalc!)
+       )
+     )
+     ;; create-pixels: number -> (listof color)
+     ;; Explanation: Creates a list of width*height color elements corresponding to perlin-noise values. Parameter is used for counting and initialized at zero
+     ;; Example: (create-pixels 0) -> [list of width*height color structs]
+     (define (create-pixels index)
+       (if (= index (* width height)) empty
+           (cons
+            (local
+              ((define x (/ (modulo index WIDTH) SCALE))
+               (define y (/ (floor (/ index WIDTH)) SCALE)))
+              (begin
+                (if (or (>= x (+ cur-x 0.5)) (= 0 x)) (inc-x! x) (if (>= y (+ cur-y 0.5)) (inc-y!) false)) ;; Borderline magic - keeps cached gradients fresh
+                (number2color (perlin-noise-on-steroids x y cur-top-left cur-top-right cur-bot-left cur-bot-right))
+              )
+            )
+            (create-pixels (+ index 1))
+           )
+       )
+     )
+    )
+    (create-pixels 0)
+  )
+)
+;; Tests
+;; (not specified)
+;;(save-image (color-list->bitmap (create-land-and-hurry WIDTH HEIGHT) WIDTH HEIGHT) "t2.png")
